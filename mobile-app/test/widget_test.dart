100e0a8b7ab2dceedf7faa3e10a91b1e0fd16834
Dani <dani.fariadossantos@heig-vd.ch>
2025-08-22 14:00:56 +0200

    wip : testing frontend pipeline

--- mobile-app/test/widget_test.dart
+++ mobile-app/test/widget_test.dart
@@ -1,9 +1,15 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter/material.dart';
-import 'package:turbodex_app/app.dart';
+
 
 void main() {
-  testWidgets('TurboDexApp renders without crashing', (WidgetTester tester) async {
+  testWidgets('App test', (WidgetTester tester) async {
 
   });
 }
